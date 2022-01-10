1ae37efa324d4172e4902da840a86ead273f562e
josetk89 <josetk_yankees@hotmail.com>
2022-01-10 13:27:11 -0500

    Esta es la base de la Pagina Web que voy a hacer del curso de JS

---
+++ salarios.js
@@ -0,0 +1,49 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Natalia",
+    salary: 1000,
+});
+colombia.push({
+    name: "Maria",
+    salary: 800,
+});
+colombia.push({
+    name: "Oscar",
+    salary: 950,
+});
+colombia.push({
+    name: "Luisa",
+    salary: 1200,
+});
+colombia.push({
+    name: "Jose",
+    salary: 5000,
+});
+colombia.push({
+    name: "Ulises",
+    salary: 2000,
+});
+colombia.push({
+    name: "Karina",
+    salary: 4500,
+});
+colombia.push({
+    name: "Sofia",
+    salary: 3200,
+});
+colombia.push({
+    name: "Helena",
+    salary: 2500,
+});
+colombia.push({
+    name: "David",
+    salary: 1500,
+});
+colombia.push({
+    name: "Katryn",
+    salary: 6000,
+});
